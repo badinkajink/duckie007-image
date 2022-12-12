82e5d724417df3572dbe8848db4a6b8d4d7343cf
KaiserZZK <zz2919@columbia.edu>
2022-12-11 22:18:24 -0500

    tested deployment on duckie007

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,8 +13,11 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+# dt-exec echo "This is an empty launch script. Update it to launch your application."
 
+roscore &
+sleep 5
+roslaunch coms3997 run_code.launch
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
